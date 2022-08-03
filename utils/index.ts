880408134117c545c6dd2b1c13ed2f2c07b0572b
Amrod92 <manlio92--@live.it>
2022-08-03 22:39:51 +0100

    Implementing Google Login Auth

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,20 @@
 import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const createOrGetUser = async (response: any) => {
+    // Google login JWT decoded
+    const decoded: { name: string, picture: string, sub: string } = jwtDecode(response.credential);
 
-export const createOrGetUser = async (response: any, addUser: any) => {
+    const { name, picture, sub } = decoded;
+     
+    // Create a user object to send to NextJS api/auth route
+    const user = {
+        _id: sub,
+        _type: 'user',
+        userName: name,
+        image: picture
+    };
 
+    // Make a POST request to NextJS API route containing the user details
+    await axios.post(`http://localhost:3000/api/auth`, user);
 };
