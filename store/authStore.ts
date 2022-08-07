f0812d1a7e31d429d85987a683c8343f7a4208dd
Amrod92 <manlio92--@live.it>
2022-08-07 22:20:06 +0100

    Showing comments on videos and suggested accounts

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,12 +2,21 @@ import create from 'zustand'
 import { persist } from 'zustand/middleware'
 import axios from 'axios'
 
+export const  BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 const authStore = (set: any) => ({
     userProfile: null,
+    allUsers: [],
 
     addUser: (user: any) => set({ userProfile: user }),
 
-    removeUser: () => set({userProfile: null})
+    removeUser: () => set({userProfile: null}),
+
+    fetchAllUsers: async () => {
+        const response = await axios.get(`${BASE_URL}/api/users`);
+
+        set({ allUsers: response.data })
+    }
 });
 
 const useAuthStore = create(
